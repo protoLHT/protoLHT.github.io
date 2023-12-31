783f873378cfe4cf154634e64fc991ac72ce3a18
protoLHT <79405545+protoLHT@users.noreply.github.com>
2023-12-31 19:17:07 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,3 @@
-// 音ファイルのパスを指定
-var audio = new Audio('click-sound.mp3');
-
-// ボタンにイベントリスナーを追加
-document.getElementById('myButton').addEventListener('click', function() {
-  audio.play(); // ボタンがクリックされた時に音を再生
-});
 // 派手な音ファイルのパスを指定
 var audio = new Audio('fancy-sound.mp3');
 
