5f2e77add94e4ad1aa03a201062bda6afdf938f8
protoLHT <79405545+protoLHT@users.noreply.github.com>
2023-12-31 19:46:31 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 // 派手な音ファイルのパスを指定
-var audio = new Audio('./Cyber15-mp3/Cyber15-1.mp3');
+var audio = new Audio('Cyber15-mp3/Cyber15-1.mp3');
 
 // すべてのボタンにイベントリスナーを追加
 document.querySelectorAll('.button').forEach(function(button) {
